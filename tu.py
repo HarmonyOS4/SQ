d1ab5f365bd178a9b1025e35db61639c4ef4a25e
HarmonyOS4 <125134947+HarmonyOS4@users.noreply.github.com>
2023-10-04 21:29:24 +0800

    Create tu.py

---
+++ tu.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+# 数据
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 6, 8, 10]
+
+# 创建图形
+plt.plot(x, y)
+
+# 添加标题和轴标签
+plt.title("My Line Plot")
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+
+# 显示图形
+plt.show()
+在这个例子中，我们定义了
